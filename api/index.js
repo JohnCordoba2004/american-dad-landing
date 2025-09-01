4149a5d0abbc73227ff3d42b66d22d5be100faf4
John Cordoba <johncordobagor@outlook.com>
2025-09-01 05:48:12 -0500

    Se hicieron actualizaciones del archivo index y el package

--- api/index.js
+++ api/index.js
@@ -3,13 +3,11 @@ import express from "express";
 import personajesRouter from "./routes/personajes.js";
 import momentosRouter from "./routes/momentos.js";
 import cors from "cors";
-import { timeStamp } from "console";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
-
 app.use(cors());
 app.use(express.json());
+
 app.use("/personajes", personajesRouter);
 app.use("/momentos", momentosRouter);
 
@@ -20,23 +18,6 @@ const personajes = JSON.parse(
 app.get("/", (req, res) => {
   res.send("API de American Dad activa 🛸");
 });
-
-// app.get("/personajes/:id", (req, res) => {
-//   const id = parseInt(req.params.id);
-//   const personaje = personajes.find((p) => p.id === id);
-
-//   if (!personaje) {
-//     return res.status(404).json({
-//       status: "error",
-//       message: "Personaje no encontrado",
-//     });
-//   }
-
-//   res.json({
-//     status: "success",
-//     data: personaje,
-//   });
-// });
 
 app.get("/stats", (req, res) => {
   const total = personajes.length;
@@ -55,6 +36,7 @@ app.get("/health", (req, res) => {
   });
 });
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () =>
   console.log(`Servidor corriendo en http://localhost:${PORT}`)
 );
